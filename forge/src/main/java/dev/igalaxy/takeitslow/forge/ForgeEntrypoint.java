07cc27b3b21bb44ecdc13c3a37bb3b79ff48570b
iGalaxyYT <contact@igalaxy.dev>
2022-08-02 17:01:13 -0700

    feat: 1.19.1 support (fabric only), fix forge 1.19

--- forge/src/main/java/dev/igalaxy/takeitslow/forge/ForgeEntrypoint.java
+++ forge/src/main/java/dev/igalaxy/takeitslow/forge/ForgeEntrypoint.java
@@ -3,7 +3,7 @@ package dev.igalaxy.takeitslow.forge;
 import dev.igalaxy.takeitslow.TakeItSlow;
 import dev.igalaxy.takeitslow.config.TakeItSlowConfig;
 import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 
@@ -12,8 +12,8 @@ public class ForgeEntrypoint {
     public ForgeEntrypoint() {
         TakeItSlow.onInitialize();
 
-        ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () ->
-                new ConfigGuiHandler.ConfigGuiFactory(
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
+                new ConfigScreenHandler.ConfigScreenFactory(
                     (mc, screen) -> AutoConfig.getConfigScreen(TakeItSlowConfig.class, screen).get()
                 )
         );
